f49cf5ae7d72b35eb12956a031e71b91122359b9
anand-shete <anandshete1234@gmail.com>
2026-04-28 02:19:52 +0530

    fix: resolved bug in pm2 config file

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -30,7 +30,7 @@ fi
 
 npm ci --omit=dev
 
-pm2 startOrReload ecosystem.config.js --only "$PM2_APP_NAME" --env production
+pm2 reload blogify --update-env || pm2 start npm --name "blogify" -- start
 pm2 save
 
 sudo nginx -s reload
